444ece003340ff16750ceb5cf030e7ba0311fbdd
AlexVipond <alex.r.vipond@gmail.com>
2020-08-20 21:37:06 -0600

    design a bit, and fix ref unwrapping

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,45 @@
 <template>
-  <div>
-    <span
-      :ref="listbox.label.ref"
-    >
-      Select a wrestler:
-    </span>
-    <button
-      :ref="listbox.button.ref"
-      class="rounded p-3 border"
-    >
-      {{ listbox.selected.value }}
-    </button>
-    <ul
-      :ref="listbox.list.ref"
-      v-show="listbox.list.isOpen.value"
-    >
-      <li
-        v-for="({ value, isActive, isSelected }) in listbox.options.values"
-        :key="value"
-        :ref="listbox.options.ref"
+  <main class="h-screen w-screen bg-purple-500 p-10">
+    <div class="rounded-md p-5 shadow-lg bg-white max-w-xl mx-auto">
+      <span
+        :ref="listbox.label.ref"
       >
-        <div 
-          class="p-3" 
-          :class="isActive.value ? 'bg-blue-600 text-white' : 'bg-white text-gray-900'"
+        Select a wrestler:
+      </span>
+      <button
+        :ref="listbox.button.ref"
+        class="rounded p-3 border"
+      >
+        {{ listbox.selected }}
+      </button>
+      <ul
+        :ref="listbox.list.ref"
+        v-show="listbox.list.isOpen"
+      >
+        <li
+          v-for="({ value, isActive, isSelected }) in listbox.options.values"
+          :key="value"
+          :ref="listbox.options.ref"
         >
-          {{ value }}
-          {{ isSelected.value }}
-        </div>
-      </li>
-    </ul>
-  </div>
+          <div 
+            class="p-3" 
+            :class="isActive ? 'bg-blue-600 text-white' : 'bg-white text-gray-900'"
+          >
+            {{ value }}
+            <!-- Heroicons checkmark -->
+            <svg
+              v-show="isSelected"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              class="ml-2 inline check w-6 h-6"
+            >
+              <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+            </svg>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
 
 <script>
